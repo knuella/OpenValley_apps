554674590e1b0a09d913f504f2fb07f7a964801f
Akigiri <Akigiri@users.noreply.github.com>
2014-09-30 21:13:34 +0200

    Create input_gpio_digital_11.py

---
+++ src/datapoint_input/input_gpio_digital_11.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as gpio
+
+from input import *
+
+class InputDPGPIOdigital(InputDP):
+
+    def __init__(self, channel):
+        InputDP.__init__(self)
+
+        self.channel = channel
+        
+        gpio.cleanup()
+        gpio.setmode(gpio.BOARD)
+        gpio.setwarnings(False)
+        gpio.setup(channel, gpio.IN)
+
+    def read_outside_value(self):    
+        try:
+            self._actual_value = gpio.input(self.channel)
+            self._state = 'good'
+        except:
+            self._state = 'bad'
+            self._actual_value = 0
+
+
+def main():
+    dp = InputDPGPIOdigital(11)
+                            
+    while True:
+        dp.run()
+
+
+if __name__ == '__main__':
+    main()
+
